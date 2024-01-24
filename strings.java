da03589829ec5380de4856bf5f17a3679fc128c7
Digvijay <digvijayshahi08@gmail.com>
2024-01-24 12:19:25 +0530

    day-07

--- strings.java
+++ strings.java
@@ -2,18 +2,37 @@ import java.util.Scanner;
 
 public class strings {
     public static void main(String args[]){
-        String name =  "digvijay";
-        String lastName = new String("shahi");
-        Scanner sc = new Scanner(System.in);
+        // String name =  "digvijay";
+        // String lastName = new String("shahi");
+        // Scanner sc = new Scanner(System.in);
         
-        String intro;
+        // String intro;
         // intro = sc.next(); Single Word as input
-        intro = sc.nextLine();// all the content 
+        // intro = sc.nextLine();// all the content 
 
        
-        System.out.println(name);
+        // System.out.println(name);
       
-        System.out.println(lastName);
-        System.out.println(intro+" "+ name+" " +lastName );
+        // System.out.println(lastName);
+        // System.out.println(intro+" "+ name+" " +lastName );
+
+
+        String a = "Tony";
+        String b = "Tony";
+        String c =  new String("Tony");
+        // if (a==c){
+        //     System.out.println("String is equal");
+        // }
+        // else{
+        //     System.out.println("String is not equal");
+        // }
+             if (a.equals(c)){
+            System.out.println("String is equal");
+        }
+        else{
+            System.out.println("String is not equal");
+        }
+
+
     }
 }
