8e07744e420b39e8cea4bcdfbf7b4ccab59aea14
Digvijay <digvijayshahi08@gmail.com>
2024-01-30 23:27:40 +0530

    day spend dong and learning birs manipuation

--- bitManipulation.java
+++ bitManipulation.java
@@ -3,5 +3,8 @@ public class bitManipulation {
         System.out.println((5&7));
         System.out.println((5|7));
         System.out.println((5^7));
+        System.out.println((~0));
+        System.out.println((5>>2));
+        System.out.println((5<<2));
     }
 }
