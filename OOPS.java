216072405349c54cb94823e5d3825ffe6a83d7e4
Digvijay <digvijayshahi08@gmail.com>
2024-02-16 22:40:49 +0530

    learn about oops pillars

--- OOPS.java
+++ OOPS.java
@@ -1,33 +1,54 @@
 public class OOPS {
-    public static void main(String[] args) {
-         Pen p = new Pen();
-         p.setcolour("Blue");
-         System.out.println(p.getClolour());
-         p.setTip(5);
-        System.out.println(p.getTip());
-        p.setcolour("Yellow");
-         System.out.println(p.getClolour());
-    }
+//     public static void main(String[] args) {
+//          Pen p = new Pen();
+//          p.setcolour("Blue");
+//          System.out.println(p.getClolour());
+//          p.setTip(5);
+//         System.out.println(p.getTip());
+//         p.setcolour("Yellow");
+//          System.out.println(p.getClolour());
+//     }
     
+// }
+// class Pen{
+//     private String color;
+//     private int tip ;
+//     // getter methods
+//     String getClolour(){
+//         return this.color;
+//     }
+    
+//     int getTip(){
+//         return this.tip;
+//     }
+
+//     //setter methods
+//     void setcolour(String newcolor){
+//         this.color = newcolor;
+
+//     }
+//     void setTip(int newtip){
+//         this.tip = newtip;
+//     }
+//inheritance
+
+public static void main(String[] args) {
+    Fish f = new Fish();
+    f.eat();
 }
-class Pen{
-    private String color;
-    private int tip ;
-    // getter methods
-    String getClolour(){
-        return this.color;
+class Animal{
+ void eat(){
+        System.out.println("animal is eating");
     }
-    
-    int getTip(){
-        return this.tip;
+ void bark (){
+        System.out.println("animal is eating");
     }
 
-    //setter methods
-    void setcolour(String newcolor){
-        this.color = newcolor;
+}
+class Fish extends Animal{
+ void swim() {
+        System.out.println("Swim");
+    }
 
-    }
-    void setTip(int newtip){
-        this.tip = newtip;
-    }
+}
     }
