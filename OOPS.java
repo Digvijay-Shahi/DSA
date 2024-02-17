b9ded1fac3827361857ebeb7e82fa5faa826993d
Digvijay <digvijayshahi08@gmail.com>
2024-02-17 22:39:52 +0530

    learn polymorphism

--- OOPS.java
+++ OOPS.java
@@ -1,54 +1,85 @@
-public class OOPS {
-//     public static void main(String[] args) {
-//          Pen p = new Pen();
-//          p.setcolour("Blue");
-//          System.out.println(p.getClolour());
-//          p.setTip(5);
-//         System.out.println(p.getTip());
-//         p.setcolour("Yellow");
-//          System.out.println(p.getClolour());
-//     }
+// import java.util.*;
+// public class OOPS {
+// //     public static void main(String[] args) {
+// //          Pen p = new Pen();
+// //          p.setcolour("Blue");
+// //          System.out.println(p.getClolour());
+// //          p.setTip(5);
+// //         System.out.println(p.getTip());
+// //         p.setcolour("Yellow");
+// //          System.out.println(p.getClolour());
+// //     }
     
+// // }
+// // class Pen{
+// //     private String color;
+// //     private int tip ;
+// //     // getter methods
+// //     String getClolour(){
+// //         return this.color;
+// //     }
+    
+// //     int getTip(){
+// //         return this.tip;
+// //     }
+
+// //     //setter methods
+// //     void setcolour(String newcolor){
+// //         this.color = newcolor;
+
+// //     }
+// //     void setTip(int newtip){
+// //         this.tip = newtip;
+// //     }
+// //inheritance
+// public static void main(String[] args) {
+//     Fish f = new Fish();
+//     f.eat();
+//     f.swim();
+
 // }
-// class Pen{
-//     private String color;
-//     private int tip ;
-//     // getter methods
-//     String getClolour(){
-//         return this.color;
+// }
+
+
+// class Animal{
+//     String color;
+//  void eat(){
+//         System.out.println("animal is eating");
 //     }
+//  void bark (){
+//         System.out.println("animal is eating");
+//     }
+
+// }
+// class Fish extends Animal{
+//     int fins;
+//  void swim() {
+//         System.out.println("Swim");
+//     }
+// }
+// class sit extends Fish{
+//      int legs;
+// }
+
+//Polymorphis
+//metod overloading
     
-//     int getTip(){
-//         return this.tip;
-//     }
-
-//     //setter methods
-//     void setcolour(String newcolor){
-//         this.color = newcolor;
-
-//     }
-//     void setTip(int newtip){
-//         this.tip = newtip;
-//     }
-//inheritance
-
-public static void main(String[] args) {
-    Fish f = new Fish();
-    f.eat();
+public class OOPS{
+    public static void main(String[] args) {
+        Calculator cl = new Calculator();
+        System.out.println(cl.sum(10, 0));
+        System.out.println(cl.sum((float)1.2,(float)2.4));
+        System.out.println(cl.sum(10, 0,4));
+    }
 }
-class Animal{
- void eat(){
-        System.out.println("animal is eating");
+class Calculator {
+    public static int sum(int a , int b) {
+        return a+b;
     }
- void bark (){
-        System.out.println("animal is eating");
+    public static float sum(float a , float b) {
+        return a+b;
     }
-
+    public static int sum(int a , int b, int c) {
+        return a+b+c;
+    }
 }
-class Fish extends Animal{
- void swim() {
-        System.out.println("Swim");
-    }
-
-}
-    }
