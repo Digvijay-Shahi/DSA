197d88b970891b3f9ef5646a31a49de0357a39a0
Digvijay <digvijayshahi08@gmail.com>
2024-03-02 10:49:38 +0530

    done recurrsion qustion

--- Recurrsion.java
+++ Recurrsion.java
@@ -46,17 +46,53 @@ public class Recurrsion {
     //     return sum;
     // }
 
-    public static int fact(int n){
-        if (n == 0 || n == 1){
-            return n;
-        }
-        int f1= fact(n-1);
-        int f2 = fact(n-2);
-        int f = f1 +f2;
-        return f;
-    }
+    // public static int fact(int n){
+    //     if (n == 0 || n == 1){
+    //         return n;
+    //     }
+    //     int f1= fact(n-1);
+    //     int f2 = fact(n-2);
+    //     int f = f1 +f2;
+    //     return f;
+    // }
+
+
+    // public static boolean isSorted(int arr[], int i){
+    //    if (i == arr.length-1){
+    //     return true;
+    //    }
+    //     if (arr[i]> arr[i+1]){
+    //         return false;
+    //     }
+    //     return isSorted(arr, i+1);
+    // } 
+
+//     public static int firstOccurance(int arr[], int key, int i){
+//         if (i==arr.length){
+//             return -1;
+//         }
+//         if (arr[i] == key){
+//             return i;
+//     }
+//    return firstOccurance(arr, key, i+1);
+//     }
+
+public static int lastOccurance(int arr[], int key, int i){
+      if (i == arr.length){
+        return -1;
+      }
+      int isfound = lastOccurance(arr, 5, i+1);
+      if(isfound == -1 && arr[i]==key){
+        return i;
+      }
+      return isfound;
+}
     public static void main(String[] args) {
-        int n = 25;
-        System.out.println(fact(n));
+        // int n = 25;
+        // System.out.println(fact(n));
+        int arr[] = {1,2,5,4,3,4,5};
+        // System.out.println(isSorted(arr, 0));
+        // System.out.println(firstOccurance(arr, 5, 0));
+        System.out.println(lastOccurance(arr, 5 ,0));
     }
 }
