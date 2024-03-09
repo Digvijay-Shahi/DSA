3a6ae1320998e11d5cf437cd2490be50e8da4b78
Digvijay <digvijayshahi08@gmail.com>
2024-03-09 13:27:28 +0530

    another day of learning

--- Recurrsion.java
+++ Recurrsion.java
@@ -77,22 +77,45 @@ public class Recurrsion {
 //    return firstOccurance(arr, key, i+1);
 //     }
 
-public static int lastOccurance(int arr[], int key, int i){
-      if (i == arr.length){
-        return -1;
-      }
-      int isfound = lastOccurance(arr, 5, i+1);
-      if(isfound == -1 && arr[i]==key){
-        return i;
-      }
-      return isfound;
-}
+// public static int lastOccurance(int arr[], int key, int i){
+//       if (i == arr.length){
+//         return -1;
+//       }
+//       int isfound = lastOccurance(arr, 5, i+1);
+//       if(isfound == -1 && arr[i]==key){
+//         return i;
+//       }
+//       return isfound;
+// }
+
+// public static int power(int x , int n){
+//     if (n == 1){
+//         return x;
+//     }
+//     int a = power(x, n-1);
+//     int ans = x * a;
+//     return ans;
+// }
+
+public static int power(int a, int n){
+    if (n == 0){
+        return 1;
+    }
+
+    int halfpower =   power(a, n/2) * power(a, n/2);
+    if (n % 2 != 0){
+        halfpower  = a * halfpower;
+    }
+    return halfpower;
+} 
     public static void main(String[] args) {
         // int n = 25;
         // System.out.println(fact(n));
-        int arr[] = {1,2,5,4,3,4,5};
+        // int arr[] = {1,2,5,4,3,4,5};
         // System.out.println(isSorted(arr, 0));
         // System.out.println(firstOccurance(arr, 5, 0));
-        System.out.println(lastOccurance(arr, 5 ,0));
+        // System.out.println(lastOccurance(arr, 5 ,0));
+        System.out.println(power(2, 2));
     }
 }
+ 
